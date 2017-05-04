5f83002d17d38188d06ce14e240ebc294683973b
JWTappert <tappertj@oregonstate.edu>
2017-05-04 14:15:03 -0700

    Adds h2 tags

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@
 require 'rack'
 
 app = Proc.new do |env|
-	['200', {'Content-Type' => 'text/html'}, ["Hello Traps and Treasures!\n"]]
+	['200', {'Content-Type' => 'text/html'}, ["<h1>Hello Traps and Treasures!</h1>\n"]]
 end
 
 Rack::Handler::WEBrick.run(app)
