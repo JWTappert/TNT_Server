7caf296ba0e2cc966c89e75339554685b524cb4f
JWTappert <tappertj@oregonstate.edu>
2017-05-16 13:27:01 -0700

    Moving to Sinatra

--- main.rb
+++ main.rb
@@ -3,10 +3,8 @@
 # Simple service for web app synch
 
 require 'rack'
+require 'sinatra'
 
-app = Proc.new do |env|
-	['200', {'Content-Type' => 'text/html'}, ["<h1>Hello Traps and Treasures!</h1>\n"]]
+get '/' do 
+	'Hello World!!'	
 end
-
-Rack::Handler::WEBrick.run(app)
-
