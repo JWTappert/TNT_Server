86b710c8947fdb6b9195ce2a9af7182913b873e6
JWTappert <tappertj@oregonstate.edu>
2017-05-20 13:39:03 -0700

    Installing redis

--- main.rb
+++ main.rb
@@ -4,7 +4,27 @@
 
 require 'rack'
 require 'sinatra'
+require 'pusher'
+require 'redis'
+
+redis = Redis.new
+
+pusher_client = Pusher::Client.new(
+  app_id: '341326',
+  key: 'db261505a706dda1d8ef',
+  secret: 'a0b9d1b0116444af68c5',
+  cluster: 'us2',
+  encrypted: true
+)
+
+
 
 get '/' do 
-	'Hello World!!'	
+	'Hello World!!'
+end
+
+get '/player' do
+  pusher_client.trigger('my-channel', 'my-event', {
+    message: 'hello world'
+  })
 end
